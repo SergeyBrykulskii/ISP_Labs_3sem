504ecf52b1f27064ec908a7ec91eee414a5e48a6
Sergey Brykulskii <serxio6758sk@gmail.com>
2022-10-31 12:01:41 +0300

    lab 7 done

--- 153505_Brykulskii_Lab7(Multithreaded programming)/153505_Brykulskii_Lab7/Program.cs
+++ 153505_Brykulskii_Lab7(Multithreaded programming)/153505_Brykulskii_Lab7/Program.cs
@@ -1,9 +1,51 @@
 ﻿using ClassLibrary;
-using System.Threading;
 
-ParametersOfDefIntegral parameters = new ParametersOfDefIntegral(0, 1, 0.00000001);
-Console.WriteLine(CountSinIntegral.CountByMethodOfRectangles(parameters));
+CountSinIntegral counter = new();
+int positionOfCursoreForResults = 10;
+int pos = 20;
+Mutex mutexObj = new();
 
-CountSinIntegral obj = new CountSinIntegral();
+counter.CountCompleted += (ts, res) =>
+{
+    Console.SetCursorPosition(0, positionOfCursoreForResults);
+    Console.Write("\r" + new string(' ', Console.BufferWidth) + "\r");
 
-Thread thread1 = new Thread(obj.CountByMethodOfRectangles);
+    Console.WriteLine($"Thread number{Thread.CurrentThread.Name} completed with: result = {res} | execution time = {ts}");
+    positionOfCursoreForResults += 1;
+};
+counter.Progress += per => {
+    mutexObj.WaitOne();
+    Console.SetCursorPosition(0, Convert.ToInt32(Thread.CurrentThread.Name) - 1);
+    Console.Write("\r" + new string(' ', Console.BufferWidth) + "\r");
+
+    Console.Write($"Thread number: {Thread.CurrentThread.Name} [");
+
+    for (int i = 0; i < per / 5; i += 1)
+        Console.Write('=');
+
+    for (int i = 0; i < 20 - per / 5; i += 1)
+        Console.Write(' ');
+
+    Console.WriteLine($"] {per}%");
+    mutexObj.ReleaseMutex();
+};
+
+Thread thread1 = new (() => counter.CountByMethodOfRectangles());
+Thread thread2 = new (() => counter.CountByMethodOfRectangles());
+Thread thread3 = new (() => counter.CountByMethodOfRectangles());
+Thread thread4 = new (() => counter.CountByMethodOfRectangles());
+Thread thread5 = new (() => counter.CountByMethodOfRectangles());
+thread1.Name = "1";
+thread2.Name = "2";
+thread3.Name = "3";
+thread4.Name = "4";
+thread5.Name = "5";
+
+thread1.Priority = ThreadPriority.Highest;
+thread2.Priority = ThreadPriority.Lowest;
+
+thread1.Start();
+thread2.Start();
+thread3.Start();
+thread4.Start();
+thread5.Start();
--- 153505_Brykulskii_Lab7(Multithreaded programming)/ClassLibrary/CountSinIntegral.cs
+++ 153505_Brykulskii_Lab7(Multithreaded programming)/ClassLibrary/CountSinIntegral.cs
@@ -1,23 +1,49 @@
-﻿namespace ClassLibrary
+﻿using System.Diagnostics;
+
+namespace ClassLibrary
 {
     public class CountSinIntegral
     {
-        public double CountByMethodOfRectangles(ParametersOfDefIntegral parameters)
+        public delegate void GetResult(TimeSpan timeSpan, decimal result);
+        public event GetResult? CountCompleted;
+
+        public delegate void ProgressBar(int per);
+        public event ProgressBar? Progress;
+
+        private Semaphore semaphore = new(2, 2);
+
+        public void CountByMethodOfRectangles(decimal step = 0.0000001m, decimal leftBorder = 0, decimal rightBorder = 1)
         {
-            double result = 0;
-            for (double i = parameters.LeftBorder; i < parameters.RightBorder; i += parameters.Step)
+
+            Stopwatch stopWatch = new();
+            semaphore.WaitOne();
+
+            stopWatch.Start();
+            decimal result = 0;
+            int lastProgress = 0;
+            
+            for (decimal i = leftBorder; i <= rightBorder; i += step)
             {
-                result += Math.Sin(i + parameters.Step / 2);
+                result += (decimal)Math.Sin((double)i) * step;
                 
-                for (int j = 0; j < 100000; j++)
+                // loop to delay the program
+                /*for (int j = 0; j < 10000000; j++)
                 {
-                    int a = 94, b = 31;
-                    a *= 31;
-                    a %= 1000;
+                    int a = j + 3;
+                }*/
+
+                int progress = Convert.ToInt32(i / rightBorder * 100);
+
+                if (progress != lastProgress )
+                {
+                    Progress?.Invoke(progress);
+                    lastProgress = progress;
                 }
             }
-
-            return result * parameters.Step;
+            
+            stopWatch.Stop();
+            CountCompleted?.Invoke(stopWatch.Elapsed, result);
+            semaphore.Release();
         }
     }
 }
